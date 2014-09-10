344e201ce753e511805adaf41353270c581520fd
Pntbr <pntbr@scopyleft.fr>
2014-09-10 14:00:13 +0200

    Inject markdown files in collection

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ bralador-contribution
 
 A module to share contents, articles, datas, posts
 
+# Dependance
 
+Posts collections
--- package.js
+++ package.js
@@ -1,8 +1,25 @@
+var fs = Npm.require('fs');
+var postFiles = [];
+
+fs.readdirSync('packages/contribution/private').forEach(
+  function (postFile) {
+    postFiles.push('private/' + postFile);
+  });
+
 Package.describe({
   summary: "A module to share contents, articles, datas, posts",
   version: "0.1.1"
 });
 
 Package.onUse(function (api, where) {
-  api.versionsFrom('METEOR@0.9.0.1');
+  api.versionsFrom('METEOR@0.9.1.1');
+  api.add_files(postFiles, ['server'], {isAsset: true});
+  api.add_files(['lib/server/inject.js'], ['server']);
+});
+
+Package.on_test(function (api) {
+  api.use('app-posts', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');
+
+  api.add_files('contribution_test.js', 'client');
 });
